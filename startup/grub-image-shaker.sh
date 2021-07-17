23e8c9fe93ae4c544d0738a7b0f98d051b4a6fbf
MrDonaldoPublic <mr.donaldo.cf@gmail.com>
2021-07-17 09:25:48 +0300

    Added searching previous image

--- startup/grub-image-shaker.sh
+++ startup/grub-image-shaker.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 IMG_DIR=/home/donaldo/GrubImage
+DESTI_FILE=/usr/share/desktop-base/active-theme/grub/grub-4x3.png
 
 images=($(find $IMG_DIR -name '*.*g*'))
+names=("${images[@]}")
 images_cnt=${#images[@]}
 
 for i in ${!images[@]}
@@ -11,6 +13,12 @@ do
     if [[ -L ${images[$i]} ]]; then
         images[$i]=$(readlink -f ${images[$i]})
         echo "is the link to ${images[$i]}"
+    fi
+    
+    difference=$(diff "${images[$i]}" $DESTI_FILE)
+    if [[ -z $difference ]]; then
+        echo "Previous Grub image was: "${images[$i]}
+        prev_img=${names[$i]}
     fi
 done
 
@@ -21,9 +29,12 @@ echo $RANDOM $index $images_cnt $image
 
 #pass=`kdialog --title "Set grub background" --password "Enter your sudo password for setting $image to grub background"`
 
-cp "$image" /usr/share/desktop-base/active-theme/grub/grub-4x3.png
+cp "$image" $DESTI_FILE
 
+name=${names[$index]}
 if [ $1 -eq 1 ]; then
-    sleep 70
+    sleep 170
 fi
-kdialog --msgbox "New grub image is "$(echo "${image##*/}")
+kdialog --msgbox "New grub image is "$(echo "${name##*/}")", previously was "$(echo "${prev_img##*/}")
+
+read
